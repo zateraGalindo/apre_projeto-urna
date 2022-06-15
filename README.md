ab29b3ac8ef138fc93729ec64967e7050f40aa67
Gabriel Galindo <zatera_@hotmail.com>
2022-06-14 22:39:35 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Projeto de aprendizado
+
+Usamos alguns dos conhecimentos ensinados nas aulas de Javascript.
---
+++ assets/js/etapas.js
@@ -0,0 +1,68 @@
+let etapas = [
+    {
+        titulo: 'VICE',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '29000',
+                name: 'Rukia Kuchiki',
+                anime: 'Bleach',
+                fotos: [
+                    {url: 'bleach-2.jpg', legenda: 'Vice'}
+                ]
+            },
+            {
+                numero: '77000',
+                name: 'Roronoa Zoro',
+                anime: 'One piece',
+                fotos: [
+                    {url: 'one-piece-2.jpg', legenda: 'Vice'}
+                ]
+            },
+            {
+                numero: '33017',
+                name: 'Nezuko Kamado',
+                anime: 'Demon Slayer',
+                fotos: [
+                    {url: 'demon-slayer-2.jpg', legenda: 'Vice'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '17',
+                name: 'Ichigo Kurosaki',
+                anime: 'Bleach',
+                vice: 'Rukia Kuchiki',
+                fotos: [
+                    {url: 'bleach.jpg', legenda: 'Capitão'},
+                    {url: 'bleach-2.jpg', legenda: 'Vice' }
+                ]
+            },
+            {
+                numero: '22',
+                name: 'Monkey D. Luffy',
+                anime: 'One Piece',
+                vice: 'Roronoa Zoro',
+                fotos: [
+                    {url: 'one-piece.jpg', legenda: 'Capitão'},
+                    {url: 'one-piece-2.jpg', legenda: 'Vice' }
+                ]
+            },
+            {
+                numero: '11',
+                name: 'Tanjirō Kamado',
+                anime: 'Demon Slayer',
+                vice: 'Nezuko Kamado',
+                fotos: [
+                    {url: 'demon-slayer.jpg', legenda: 'Capitão'},
+                    {url: 'demon-slayer-2.jpg', legenda: 'Vice' }
+                ]
+            },
+        ]
+    }
+]
